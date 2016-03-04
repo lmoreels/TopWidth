8fa7e39a13c2a9d90a792f23e8dbfc20dc926d35
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2016-03-04 11:39:04 +0100

    Load 76X libraries and add possibility to compile only one macro

--- compile.sh
+++ compile.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-for ccfile in ./*.cc
-do
+# if there is one arg compile only this .cc
+if [[ -n $1 ]]
+then
+    ccfile=$1
+
     ofile=`echo $ccfile |sed 's/\.cc$//g'`
     echo "compiling : " $ccfile ", executible name: " $ofile
-    g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent74 -l TopTreeAna74 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
-#    g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
-done
+    g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
+
+
+# if there is no arg compile all .cc
+else
+    for ccfile in ./*.cc
+    do
+        ofile=`echo $ccfile |sed 's/\.cc$//g'`
+        echo "compiling : " $ccfile ", executible name: " $ofile
+        g++ -g -std=c++11 -L ~/lib -L . -L .. -I ./ -I ../ -l TopTreeAnaContent76 -l TopTreeAna76 -l MLP -l TreePlayer -l TMVA -l XMLIO -I `root-config --incdir` `root-config --libs` $ccfile -o $ofile
+    done
+fi
