78348f99b75f28a0630b471efa4a6bc0bc7a6fc7
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2016-03-25 10:06:41 +0100

    Added fit function to TransferFunctions code
    - Fitting of resolution histograms
    - Parameters written to table
    - Compiles, but still to be tested in analysis code

--- TransferFunctions/TransferFunctions.h
+++ TransferFunctions/TransferFunctions.h
@@ -14,13 +14,20 @@
 #include <TMath.h>
 #include <Math/GenVector/VectorUtil.h>
 #include <iostream>
+#include <ostream>
 #include <fstream>
 #include <sstream>
+//#include <ofstream>
+#include <string>
 #include <sys/stat.h>
 #include <errno.h>
 #include <map>
+#include <array>
 #include <vector>
+#include <TH1.h>
 #include <TH2.h>
+#include <TF1.h>
+#include <TFile.h>
 #include <TLorentzVector.h>
 
 
@@ -28,19 +35,24 @@ class TransferFunctions{
   public:
     TransferFunctions(bool calculateTransferFunctions);
     ~TransferFunctions();
+    std::string toStr(int number);
     void bookHistograms();
     void fillJets(std::vector<TLorentzVector*> parton, std::vector<TLorentzVector*> jet);
     void fillMuon(TLorentzVector *genMu, TLorentzVector *recMu);
     void fillElectron(TLorentzVector *genEl, TLorentzVector *recEl);
-    void writeOutputFiles();
-    void writeTable();
+    void writeHistograms();
+    void makeFit();
+    void writeTable(std::string inputFileName);
 
     
   private:
     bool muon;
     bool electron;
     std::map<std::string,TH2F*> histoTr2D;
-    std::string filename;
+    std::string inputFileName;
+    int nHistos;
+    static const std::string histoNames[];
+    static const std::string histoDescription[];
 
 };
 
