8b61d77f3822298ba0fd13e94bf4d3c1411d512f
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2016-08-18 12:20:19 +0200

    Also calculate average top mass for all chi2 samples combined

--- macros/AverageMassCalculator.cc
+++ macros/AverageMassCalculator.cc
@@ -22,24 +22,29 @@ string thisDataSet;
 /// Define functions
 bool fexists(const char *filename);	// check if file exists
 void ClearVars(bool isNewDataset);
+void WriteToFile(std::ofstream &fout, std::string thisDataSet, double meanW, double meanTop);
 
 /// Define vars
 char dataLine[1024];
-int nEntries, eventId;
+int nEntries, nEntriesChi2, eventId;
 double massW, massTop;
-double sumW, sumTop;
+double sumW, sumTop, sumWChi2, sumTopChi2;
 double meanW, meanTop;
+
+ifstream fileIn;
+streampos currentPosition;
+ofstream fileOut;
 
 int main()
 {
-  ifstream fileIn;
-  streampos currentPosition;
-  
-  ofstream fileOut;
   string outputFileName = pathInput+"averageMass_"+inputDate+".txt";
   fileOut.open(outputFileName.c_str());
   cout << "Creating output file " << outputFileName << "..." << endl;
   fileOut << "# Dataset       meanW    meanTop" << endl;
+  
+  nEntriesChi2 = 0;
+  sumWChi2 = 0.;
+  sumTopChi2 = 0.;
   
   for (int iFile = 0; iFile < nInputs; iFile++)
   {
@@ -72,6 +77,12 @@ int main()
       sumW += massW;
       sumTop += massTop;
       
+      if ( iFile > 0 ) // chi2
+      {
+        nEntriesChi2++;
+        sumWChi2 += massW;
+        sumTopChi2 += massTop;
+      }
       
     }  // end while
     
@@ -95,17 +106,20 @@ int main()
       thisDataSet = dataSetNames[0]+" chi2";
     }
     
-    fileOut << left << setw(13) << thisDataSet;
-    cout.setf(ios::fixed,ios::floatfield);  // Add zero to obtain the asked number of digits
-    cout.precision(3);  // setprecision(3) --> set maximum number of meaningful digits to 3. When 'fixed' or 'scientific', only count after decimal point.
-    fileOut << "   " << fixed << showpoint << setprecision(3) << meanW;  // showpoint --> also when decimal part is zero
-    fileOut << "   " << fixed << showpoint << setprecision(3) << meanTop << endl;
+    WriteToFile(fileOut, thisDataSet, meanW, meanTop);
     
     
     /// Close input file
     fileIn.close();
     
   }  // end loop files
+  
+  /// Calculate mean all chi2
+  ClearVars(true);
+  meanW = sumWChi2/((double)nEntriesChi2);
+  meanTop = sumTopChi2/((double)nEntriesChi2);
+  
+  WriteToFile(fileOut, "All Chi2", meanW, meanTop);
   
   
   /// Close output file
@@ -138,4 +152,13 @@ void ClearVars(bool isNewDataset)
     meanW = 0;
     meanTop = 0;
   }
+}
+
+void WriteToFile(std::ofstream &fout, std::string thisDataSet, double meanW, double meanTop)
+{
+  fout << left << setw(13) << thisDataSet;
+  cout.setf(ios::fixed,ios::floatfield);  // Add zero to obtain the asked number of digits
+  //cout.precision(3);  // setprecision(3) --> set maximum number of meaningful digits to 3. When 'fixed' or 'scientific', only count after decimal point.
+  fout << "   " << fixed << showpoint << setprecision(3) << meanW;  // showpoint --> also when decimal part is zero
+  fout << "   " << fixed << showpoint << setprecision(3) << meanTop << endl;
 }
