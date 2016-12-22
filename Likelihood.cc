c87b4f732416ff1229becb497cc56bc8fe10d24b
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2016-12-22 17:35:29 +0100

    Created likelihood function, debugging needed

--- Likelihood.cc
+++ Likelihood.cc
@@ -24,32 +24,24 @@ using namespace std;
 bool test = true;
 bool runLocally = false;
 
-/// Define vars
-float width[] = {0.25, 0.33, 0.5, 1., 2., 3., 4.};
-string widthStr[] = {"0p25", "0p33", "0p5", "1", "2", "3", "4"};
-int nWidths = sizeof(widthStr)/sizeof(widthStr[0]);
-vector<double> muVoigt, sigmaVoigt, gammaVoigt, rVoigt, normVoigt;
-vector<double> muErrVoigt, sigmaErrVoigt, gammaErrVoigt, rErrVoigt, normErrVoigt;
-vector<double> alphaCB_WP, nCB_WP, sigmaCB_WP, muCB_WP, normCB_WP;
-vector<double> alphaErrCB_WP, nErrCB_WP, sigmaErrCB_WP, muErrCB_WP, normErrCB_WP;
-vector<double> alphaCB_UP, nCB_UP, sigmaCB_UP, muCB_UP, normCB_UP;
-vector<double> alphaErrCB_UP, nErrCB_UP, sigmaErrCB_UP, muErrCB_UP, normErrCB_UP;
-int paramNb;
-string paramName;
-double paramValue, paramError;
+const int nCP = 588412;
+const int nWP = 1064848;
+const int nUP = 1648250;
 
-char dataLine[1024];
-string inputFileName;
-ifstream fileIn;
-streampos currentPosition;
+const double mu_CP = 0.9984, sigma_CP = 0.08913, r_CP = 2.47, norm_CP = 0.002558;
+const double alpha_WP = -0.3614, n_WP = 20, sigma_WP = 0.1278, mu_WP = 0.7436, norm_WP = 0.004463;
+const double alpha_UP = -0.3639, n_UP = 20, sigma_UP = 0.1439, mu_UP = 0.7167, norm_UP = 0.003993;
+
+/// Define vars
+int nTot;
+double f_CP, f_WP, f_UP;
+
 
 /// Define functions
 string ConvertIntToString(int Number, int pad);
 string MakeTimeStamp();
-bool fexists(const char *filename);	// check if file exists
-bool ClearVectors();
 bool ClearVars();
-Double_t voigt(Double_t *x, Double_t *par)
+Double_t voigt(Double_t *x, Double_t *par);
 Double_t crysBall(Double_t *x, Double_t *par);
 
 
@@ -63,195 +55,37 @@ int main (int argc, char *argv[])
   cout << "********************************" << endl;
   
   ClearVars();
-  ClearVectors();
   
-  string pathInput = "/user/lmoreels/CMSSW_7_6_5/src/TopBrussels/TopWidth/OutputVoigt/";
-  if (runLocally) pathInput = "/Users/lmoreels/cernbox/TopWidth/TopTrees/tempPlots/Voigt/161220/";
+  /// Fraction of events that is CP, WP and UP
+  nTot = nCP + nWP + nUP;
+  f_CP = (double)nCP/(double)nTot;
+  f_WP = (double)nWP/(double)nTot;
+  f_UP = (double)nUP/(double)nTot;
   
-  /// Get fit parameters from files
-  for (int iWidth = 0; iWidth < nWidths; iWidth++)
-  {
-    /// Open input file
-    inputFileName = "FitParams_widthx"+widthStr[iWidth]+".txt";
-    if (! fexists((pathInput+inputFileName).c_str()) )
-    {
-      cout << "WARNING: File " << inputFileName << " does not exist." << endl;
-      continue;
-    }
-    fileIn.open((pathInput+inputFileName).c_str());
-    cout << "Opening " << inputFileName << "..." << endl;
-    fileIn.getline(dataLine,sizeof(dataLine));
-    currentPosition = fileIn.tellg();
-    
-    /// Loop over input file
-    do // until file ends
-    {
-      //cout << dataLine << endl;
-      if ( /*string(dataLine).find("Voigt") != std::string::npos && */ string(dataLine).find("CP") != std::string::npos )
-      {
-        cout << "  - " << dataLine << ": Adding parameter values for width " << widthStr[iWidth] << "." << endl;
-        ClearVars();
-        
-        fileIn.seekg(currentPosition);
-        fileIn.getline(dataLine,sizeof(dataLine));
-        currentPosition = fileIn.tellg();
-        
-        do // until empty line
-        {
-          istringstream iss(dataLine);
-          iss >> paramNb >> paramName >> paramValue >> paramError;
-          if ( paramNb == 0 ) // mu
-          {
-            muVoigt   .push_back(paramValue);
-            muErrVoigt.push_back(paramError);
-          }
-          if ( paramNb == 1 ) // sigma
-          {
-            sigmaVoigt   .push_back(paramValue);
-            sigmaErrVoigt.push_back(paramError);
-          }
-          if ( paramNb == 2 ) // gamma
-          {
-            gammaVoigt   .push_back(paramValue);
-            gammaErrVoigt.push_back(paramError);
-          }
-          if ( paramNb == 3 ) // r
-          {
-            rVoigt   .push_back(paramValue);
-            rErrVoigt.push_back(paramError);
-          }
-          if ( paramNb == 4 ) // norm
-          {
-            normVoigt   .push_back(paramValue);
-            normErrVoigt.push_back(paramError);
-          }
-          
-          fileIn.seekg(currentPosition);
-          fileIn.getline(dataLine,sizeof(dataLine));
-          currentPosition = fileIn.tellg();
-          
-        } while (! string(dataLine).empty() );
-        
-      }  // end Voigt CP
-      else if ( string(dataLine).find("CrystalBall") != std::string::npos && string(dataLine).find("WP") != std::string::npos && string(dataLine).find("UP") == std::string::npos && string(dataLine).find("Ok") == std::string::npos)
-      {
-        cout << "  - " << dataLine << ": Adding parameter values for width " << widthStr[iWidth] << "." << endl;
-        ClearVars();
-        
-        fileIn.seekg(currentPosition);
-        fileIn.getline(dataLine,sizeof(dataLine));
-        currentPosition = fileIn.tellg();
-        
-        do // until empty line
-        {
-          istringstream iss(dataLine);
-          iss >> paramNb >> paramName >> paramValue >> paramError;
-          if ( paramNb == 0 ) // alpha
-          {
-            alphaCB_WP   .push_back(paramValue);
-            alphaErrCB_WP.push_back(paramError);
-          }
-          if ( paramNb == 1 ) // n
-          {
-            nCB_WP   .push_back(paramValue);
-            nCB_WP.push_back(paramError);
-          }
-          if ( paramNb == 2 ) // sigma
-          {
-            sigmaCB_WP   .push_back(paramValue);
-            sigmaErrCB_WP.push_back(paramError);
-          }
-          if ( paramNb == 3 ) // mu
-          {
-            muCB_WP   .push_back(paramValue);
-            muErrCB_WP.push_back(paramError);
-          }
-          if ( paramNb == 4 ) // norm
-          {
-            normCB_WP   .push_back(paramValue);
-            normErrCB_WP.push_back(paramError);
-          }
-          
-          fileIn.seekg(currentPosition);
-          fileIn.getline(dataLine,sizeof(dataLine));
-          currentPosition = fileIn.tellg();
-          
-        } while (! string(dataLine).empty() );
-        
-      }  // end CB WP
-      else if ( string(dataLine).find("CrystalBall") != std::string::npos && string(dataLine).find("UP") != std::string::npos && string(dataLine).find("WP") == std::string::npos  )
-      {
-        cout << "  - " << dataLine << ": Adding parameter values for width " << widthStr[iWidth] << "." << endl;
-        ClearVars();
-        
-        fileIn.seekg(currentPosition);
-        fileIn.getline(dataLine,sizeof(dataLine));
-        currentPosition = fileIn.tellg();
-        
-        do // until empty line
-        {
-          istringstream iss(dataLine);
-          iss >> paramNb >> paramName >> paramValue >> paramError;
-          if ( paramNb == 0 ) // alpha
-          {
-            alphaCB_UP   .push_back(paramValue);
-            alphaErrCB_UP.push_back(paramError);
-          }
-          if ( paramNb == 1 ) // n
-          {
-            nCB_UP   .push_back(paramValue);
-            nCB_UP.push_back(paramError);
-          }
-          if ( paramNb == 2 ) // sigma
-          {
-            sigmaCB_UP   .push_back(paramValue);
-            sigmaErrCB_UP.push_back(paramError);
-          }
-          if ( paramNb == 3 ) // mu
-          {
-            muCB_UP   .push_back(paramValue);
-            muErrCB_UP.push_back(paramError);
-          }
-          if ( paramNb == 4 ) // norm
-          {
-            normCB_UP   .push_back(paramValue);
-            normErrCB_UP.push_back(paramError);
-          }
-          
-          fileIn.seekg(currentPosition);
-          fileIn.getline(dataLine,sizeof(dataLine));
-          currentPosition = fileIn.tellg();
-          
-        } while (! string(dataLine).empty() );
-        
-      }  // end CB UP
-      
-      fileIn.seekg(currentPosition);
-      fileIn.getline(dataLine,sizeof(dataLine));
-      currentPosition = fileIn.tellg();
-    
-    } while ( fileIn.good() );
-    
-    /// Close input file
-    fileIn.close();
-    
-  }  // end loop widths (to get fit params)
-  
-  if (test)
-  {
-    cout << "Widths: " << width[0] << "  " << width[1] << "  " << width[2] << "  " << width[3] << "  " << width[4] << "  " << width[5] << "  " << width[6] << endl;
-    cout << "muVoigts: " << muVoigt[0] << "  " << muVoigt[1] << "  " << muVoigt[2] << "  " << muVoigt[3] << "  " << muVoigt[4] << "  " << muVoigt[5] << "  " << muVoigt[6] << endl;
-    cout << "muErrVoigts: " << muErrVoigt[0] << "  " << muErrVoigt[1] << "  " << muErrVoigt[2] << "  " << muErrVoigt[3] << "  " << muErrVoigt[4] << "  " << muErrVoigt[5] << "  " << muErrVoigt[6] << endl;
-    cout << "muCB_WPs: " << muCB_WP[0] << "  " << muCB_WP[1] << "  " << muCB_WP[2] << "  " << muCB_WP[3] << "  " << muCB_WP[4] << "  " << muCB_WP[5] << "  " << muCB_WP[6] << endl;
-    cout << "muErrCB_WPs: " << muErrCB_WP[0] << "  " << muErrCB_WP[1] << "  " << muErrCB_WP[2] << "  " << muErrCB_WP[3] << "  " << muErrCB_WP[4] << "  " << muErrCB_WP[5] << "  " << muErrCB_WP[6] << endl;
-    cout << "muCB_UPs: " << muCB_UP[0] << "  " << muCB_UP[1] << "  " << muCB_UP[2] << "  " << muCB_UP[3] << "  " << muCB_UP[4] << "  " << muCB_UP[5] << "  " << muCB_UP[6] << endl;
-    cout << "muErrCB_UPs: " << muErrCB_UP[0] << "  " << muErrCB_UP[1] << "  " << muErrCB_UP[2] << "  " << muErrCB_UP[3] << "  " << muErrCB_UP[4] << "  " << muErrCB_UP[5] << "  " << muErrCB_UP[6] << endl;
-  }
   
   /// LIKELIHOOD ///
   // prob = f_CP * Voigt + f_WP * CB + f_UP * CB
   // likelihood = - log (prob)
   
+  //TF1 *function = new TF1("function", func, -1, 3, 15);
+  //function->SetParameters(mu_CP, sigma_CP, 1., r_CP, norm_CP, alpha_WP, n_WP, sigma_WP, mu_WP, norm_WP, alpha_UP, n_UP, sigma_UP, mu_UP, norm_UP);
+  
+  //TFormula *function = new TFormula("function", f_CP*Voigt());
+  
+  TF1 *crysBall_WP = new TF1("crysBall_WP",crysBall);
+  crysBall_WP->SetParameters(alpha_WP, n_WP, sigma_WP, mu_WP, norm_WP);
+  TF1 *crysBall_UP = new TF1("crysBall_UP",crysBall);
+  crysBall_UP->SetParameters(alpha_UP, n_UP, sigma_UP, mu_UP, norm_UP);
+  
+  TF1 *voigt_CP = new TF1("voigt_CP",voigt);
+  voigt_CP->SetParameter(0, mu_CP);
+  voigt_CP->SetParameter(1, sigma_CP);
+  voigt_CP->SetParameter(3, r_CP);
+  voigt_CP->SetParameter(4, norm_CP);
+  
+  TF1 *likelihood = new TF1("likelihood", "-TMath::Log( f_CP*voigt_CP + f_WP*crysBall_WP + f_UP*crysBall_UP )" );
+  
+  cout << "Created log likelihood function!" << endl;
   
   return 0;
 }
@@ -290,39 +124,12 @@ string MakeTimeStamp()
   return date_str;
 }
 
-bool fexists(const char *filename)
-{
-  ifstream ifile(filename);
-  return ifile;
-}
-
-bool ClearVectors()
-{
-  muVoigt.clear(); muErrVoigt.clear();
-  sigmaVoigt.clear(); sigmaErrVoigt.clear();
-  gammaVoigt.clear(); gammaErrVoigt.clear();
-  rVoigt.clear(); rErrVoigt.clear();
-  normVoigt.clear(); normErrVoigt.clear();
-  
-  alphaCB_WP.clear(); alphaErrCB_WP.clear();
-  nCB_WP.clear(); nErrCB_WP.clear();
-  sigmaCB_WP.clear(); sigmaErrCB_WP.clear();
-  muCB_WP.clear(); muErrCB_WP.clear();
-  normCB_WP.clear(); normErrCB_WP.clear();
-  
-  alphaCB_UP.clear(); alphaErrCB_UP.clear();
-  nCB_UP.clear(); nErrCB_UP.clear();
-  sigmaCB_UP.clear(); sigmaErrCB_UP.clear();
-  muCB_UP.clear(); muErrCB_UP.clear();
-  normCB_UP.clear(); normErrCB_UP.clear();
-}
-
 bool ClearVars()
 {
-  paramNb = -1;
-  paramName = "";
-  paramValue = 9999.;
-  paramError = 9999.;
+  nTot = -1.;
+  f_CP = -1.;
+  f_WP = -1.;
+  f_UP = -1.;
 
 }
 
