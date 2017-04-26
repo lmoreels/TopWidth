26d423cb0d1003906fffb38f7225b57915c60f4c
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2017-04-26 16:02:56 +0200

    Run over input histos with different top widths
    - bool to run 1 file or multiple
    - Plot total probability for different widths

--- MakeTGraphFromHisto.cc
+++ MakeTGraphFromHisto.cc
@@ -22,9 +22,13 @@
 using namespace std;
 
 
-string inputDate = "170425_1455/";
-string suffix = "";
-map<string,TH1F*> histo;
+bool useDifferentWidths = true;
+
+string inputDate = "170426_0952";
+string suffix = "_90b";
+pair<string,string> input[] = { pair<string,string>("0p5","170426_0949"), pair<string,string>("0p66","170426_0950"), pair<string,string>("0p75","170426_0951"), pair<string,string>("1","170426_0952"), pair<string,string>("2","170426_0956"), pair<string,string>("3","170426_0957"), pair<string,string>("4","170426_0958") };
+
+map<string,TH1F*> histo, histoTotal;
 map<string,TGraph*> graph;
 
 /// Functions
@@ -33,10 +37,12 @@ string MakeTimeStamp();
 bool fexists(const char *filename);
 void ClearVars();
 void DrawGraph(TH1F* h, TGraph* g, string name);
+void DrawLikelihoods(double nWidths, pair<string,string> *input);
 void WriteOutput(int nPoints, double *arrayCentre, double *arrayContent);
 
 
 int binMin, binMax;
+string histoName = "";
 ofstream txtOutput;
 
 
@@ -49,10 +55,8 @@ int main (int argc, char *argv[])
   cout << "* Current time: " << dateString << "    *" << endl;
   cout << "********************************" << endl;
   
-  string inputFileName = "/user/lmoreels/CMSSW_7_6_5/src/TopBrussels/TopWidth/OutputPlots/mu/"+inputDate+"NtuplePlots_nominal.root";
-  string outputFileName = "/user/lmoreels/CMSSW_7_6_5/src/TopBrussels/TopWidth/TGraphFits/GraphFunctions"+suffix+".root";
   
-  TFile *fileIn = new TFile(inputFileName.c_str(),"read");
+  string outputFileName = "/user/lmoreels/CMSSW_7_6_5/src/TopBrussels/TopWidth/TGraphFits/GraphFunctions.root";
   TFile *fileOut = new TFile(outputFileName.c_str(),"recreate");
   
   string listCats[] = {"CP", "WP", "UP"};
@@ -66,89 +70,123 @@ int main (int argc, char *argv[])
   std::array<std::vector<double>, sizeCats> vBinCentres;
   std::array<std::vector<double>, sizeCats> vBinContents;
   
-  TH1F *histoTotal;
+  double nWidths = 1;
+  if (useDifferentWidths) nWidths = sizeof(input)/sizeof(input[0]);
+  cout << "Running over " << nWidths << " widths" << endl;
   
-  
-  for (int iCat = 0; iCat < sizeCats; iCat++)
+  for (int iWidth = 0; iWidth < nWidths; iWidth++)
   {
-    ClearVars();
-    
-    histo[listCats[iCat]] = (TH1F*) fileIn->Get(("1D_histograms/Red_top_mass_"+listCats[iCat]+"_60b").c_str());
-    histo[listCats[iCat]]->Smooth();
-    nBins[iCat] = histo[listCats[iCat]]->GetNbinsX();
-    if ( iCat > 0 && nBins[iCat] != nBins[iCat-1] )
+    if (useDifferentWidths)
     {
-      cerr << "Histograms should have the same number of bins! Exiting..." << endl;
-      exit(1);
+      suffix = "_widthx"+input[iWidth].first+"_90b";
+      inputDate = input[iWidth].second;
     }
     
-    binMin = histo[listCats[iCat]]->FindBin(0.6);
-    binMax = histo[listCats[iCat]]->FindBin(1.4);
-    for (int iBin = binMin; iBin < binMax+1; iBin++) { nEvents[iCat] += histo[listCats[iCat]]->GetBinContent(iBin);}
-    totEvents += nEvents[iCat];
+    string inputFileName = "/user/lmoreels/CMSSW_7_6_5/src/TopBrussels/TopWidth/OutputPlots/mu/"+inputDate+"/NtuplePlots_nominal.root";
+    TFile *fileIn = new TFile(inputFileName.c_str(),"read");
     
-    cout << listCats[iCat] << ": " << nEvents[iCat] << " events" << endl;
+    fileOut->cd();
     
-    /// Normalise histo on relevant subdomain
-    Double_t integral = histo[listCats[iCat]]->Integral(binMin, binMax);
-    histo[listCats[iCat]]->Scale(1./integral);
-    
-    /// Get bin centres & contents
-    for (int iBin = binMin; iBin < binMax+1; iBin++)
+    totEvents = 0;
+    for (int i = 0; i < sizeCats; i++)
     {
-      (vBinCentres[iCat]).push_back(histo[listCats[iCat]]->GetBinCenter(iBin));
-      (vBinContents[iCat]).push_back(histo[listCats[iCat]]->GetBinContent(iBin));
+      nBins[i] = 0;
+      nEvents[i] = 0;
+      fracCats[i] = 0.;
+      vBinCentres[i].clear();
+      vBinContents[i].clear();
     }
     
-  }  /// end cats
-  
-  cout << "Total: " << totEvents << " events" << endl;
-  
-  /// Make arrays as input for TGraph
-  const int nPoints = vBinCentres[0].size();
-  double binCentreArray[nPoints] = {0.}, binContentArray[sizeCats][nPoints] = {0.}, totalBinContentArray[nPoints] = {0.};
-  for (int i = 0; i < nPoints; i++)
-  {
-    binCentreArray[i] = vBinCentres[0][i];
     for (int iCat = 0; iCat < sizeCats; iCat++)
     {
-      binContentArray[iCat][i] = vBinContents[iCat][i];
-      // Calculate event fractions (one time)
-      if ( i == 0 ) { fracCats[iCat] = (double)nEvents[iCat]/(double)totEvents;}
-    }
-  }
-  
-  /// Make TGraphs
-  for (int iCat = 0; iCat < sizeCats; iCat++)
-  {
-    graph[listCats[iCat]] = new TGraph(nPoints, binCentreArray, binContentArray[iCat]);
-    graph[listCats[iCat]]->Write();
-    DrawGraph(histo[listCats[iCat]], graph[listCats[iCat]], "Graph_Red_top_mass_"+listCats[iCat]);
+      ClearVars();
+      
+      histoName = listCats[iCat]+suffix;
+      
+      histo[histoName] = (TH1F*) fileIn->Get(("1D_histograms/Red_top_mass_"+listCats[iCat]+"_90b").c_str());
+      histo[histoName]->Smooth();
+      nBins[iCat] = histo[histoName]->GetNbinsX();
+      if ( iCat > 0 && nBins[iCat] != nBins[iCat-1] )
+      {
+        cerr << "Histograms should have the same number of bins! Exiting..." << endl;
+        exit(1);
+      }
+      
+      binMin = histo[histoName]->FindBin(0.6);
+      binMax = histo[histoName]->FindBin(1.4);
+      for (int iBin = binMin; iBin < binMax+1; iBin++) { nEvents[iCat] += histo[histoName]->GetBinContent(iBin);}
+      totEvents += nEvents[iCat];
+      
+      cout << histoName << ": " << nEvents[iCat] << " events" << endl;
+      
+      /// Normalise histo on relevant subdomain
+      Double_t integral = histo[histoName]->Integral(binMin, binMax);
+      histo[histoName]->Scale(1./integral);
+      
+      /// Get bin centres & contents
+      for (int iBin = binMin; iBin < binMax+1; iBin++)
+      {
+        (vBinCentres[iCat]).push_back(histo[histoName]->GetBinCenter(iBin));
+        (vBinContents[iCat]).push_back(histo[histoName]->GetBinContent(iBin));
+      }
+      
+    }  /// end cats
     
-    fracCats[iCat] = (double)nEvents[iCat]/(double)totEvents;
-    for (int i = 0; i < nPoints; i++) totalBinContentArray[i] += fracCats[iCat]*binContentArray[iCat][i];
+    cout << "Total: " << totEvents << " events" << endl;
     
-    histo[listCats[iCat]]->Scale(fracCats[iCat]);
-    if ( iCat == 0 )
+    /// Make arrays as input for TGraph
+    const int nPoints = vBinCentres[0].size();
+    double binCentreArray[nPoints] = {0.}, binContentArray[sizeCats][nPoints] = {0.}, totalBinContentArray[nPoints] = {0.};
+    for (int i = 0; i < nPoints; i++)
     {
-      histoTotal = (TH1F*) histo[listCats[iCat]]->Clone("SumOfCats");
-      histoTotal->SetTitle("#frac{n_{CP}}{n_{tot}} * f_{CP}(x|#Gamma) + #frac{n_{WP}}{n_{tot}} * f_{WP}(x|#Gamma) + #frac{n_{UP}}{n_{tot}} * f_{UP}(x|#Gamma)");
+      binCentreArray[i] = vBinCentres[0][i];
+      for (int iCat = 0; iCat < sizeCats; iCat++)
+      {
+        binContentArray[iCat][i] = vBinContents[iCat][i];
+        // Calculate event fractions (one time)
+        if ( i == 0 ) { fracCats[iCat] = (double)nEvents[iCat]/(double)totEvents;}
+      }
     }
-    else histoTotal->Add(histo[listCats[iCat]]);
-  }
+    
+    /// Make TGraphs
+    for (int iCat = 0; iCat < sizeCats; iCat++)
+    {
+      histoName = listCats[iCat]+suffix;
+      
+      graph[histoName] = new TGraph(nPoints, binCentreArray, binContentArray[iCat]);
+      graph[histoName]->Write();
+      DrawGraph(histo[histoName], graph[histoName], "Graph_Red_top_mass_"+histoName);
+
+      fracCats[iCat] = (double)nEvents[iCat]/(double)totEvents;
+      for (int i = 0; i < nPoints; i++) totalBinContentArray[i] += fracCats[iCat]*binContentArray[iCat][i];
+
+      histo[histoName]->Scale(fracCats[iCat]);
+      if ( iCat == 0 )
+      {
+        histoTotal[suffix] = (TH1F*) histo[histoName]->Clone("TotalProbability");
+        histoTotal[suffix]->SetTitle("#frac{n_{CP}}{n_{tot}} * f_{CP}(x|#Gamma) + #frac{n_{WP}}{n_{tot}} * f_{WP}(x|#Gamma) + #frac{n_{UP}}{n_{tot}} * f_{UP}(x|#Gamma)");
+      }
+      else histoTotal[suffix]->Add(histo[histoName]);
+    }
+    
+    cout << "The integral of the weighted likelihood histogram is " << histoTotal[suffix]->Integral(binMin, binMax) << endl;
+    
+    /// Make likelihood function
+    graph["likelihood"+suffix] = new TGraph(nPoints, binCentreArray, totalBinContentArray);
+    graph["likelihood"+suffix]->Write();
+    DrawGraph(histoTotal[suffix], graph["likelihood"+suffix], "Graph_likelihood"+suffix);
+    
+    WriteOutput(nPoints, binCentreArray, totalBinContentArray);
+    
+    /// Close input file
+    fileIn->Close();
+  }  // end nWidths
   
-  cout << "The integral of the weighted likelihood histogram is " << histoTotal->Integral(binMin, binMax) << endl;
+  if (useDifferentWidths) DrawLikelihoods(nWidths, input);
   
-  /// Make likelihood function
-  graph["likelihood"] = new TGraph(nPoints, binCentreArray, totalBinContentArray);
-  DrawGraph(histoTotal, graph["likelihood"], "Graph_likelihood");
-  
-  WriteOutput(nPoints, binCentreArray, totalBinContentArray);
-  
-  
-  /// Close files
+  /// Close output file
   fileOut->Close();
-  fileIn->Close();
+  
   
   return 0;
 }
@@ -196,6 +234,7 @@ void ClearVars()
 {
   binMin = 0;
   binMax = 100;
+  histoName = "";
 }
 
 void DrawGraph(TH1F* h, TGraph* g, string name)
@@ -208,7 +247,28 @@ void DrawGraph(TH1F* h, TGraph* g, string name)
   g->Draw("same");
   c->Update();
   c->Write();
-  c->SaveAs((name+".png").c_str());
+  c->SaveAs(("TGraphFits/"+name+".png").c_str());
+}
+
+void DrawLikelihoods(double nWidths, pair<string,string> *input)
+{
+  Color_t colours[] = {kRed, kOrange-3, kYellow-7, kGreen-7, kGreen+1, kCyan+1, kBlue+2, kMagenta, kViolet-5, kPink+10};
+  TCanvas* c2 = new TCanvas("TotalProbability", "TotalProbability");
+  c2->cd();
+  graph["likelihood_widthx"+input[0].first+"_90b"]->SetLineColor(colours[0]);
+  graph["likelihood_widthx"+input[0].first+"_90b"]->Draw();
+  c2->Update();
+  for (int i = 1; i < nWidths; i++)
+  {
+    graph["likelihood_widthx"+input[i].first+"_90b"]->SetLineColor(colours[i%10]);
+    graph["likelihood_widthx"+input[i].first+"_90b"]->Draw("same");
+    c2->Update();
+  }
+  c2->Write();
+  c2->SaveAs("TGraphFits/Probabilities_90b.png");
+  c2->Close();
+  
+  delete c2;
 }
 
 void WriteOutput(int nPoints, double *arrayCentre, double *arrayContent)
