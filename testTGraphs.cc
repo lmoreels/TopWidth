e700be89a68681576dd77ff01b47a755fdbf66c0
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2017-05-02 19:00:32 +0200

    Take -log of probability function
    - Write loglikelihood graph to separate file
    - Add more width plots
    - Set maxBin to GetBin(1.4)+1

---
+++ testTGraphs.cc
@@ -0,0 +1,136 @@
+////////////////////////////////////////////////////////////
+/////                                                  /////
+/////  Preliminary macro to try and make some plots    /////
+/////                                                  /////
+////////////////////////////////////////////////////////////
+
+
+
+#include "TStyle.h"
+#include <stdio.h>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <sys/stat.h>
+#include <map>
+#include <array>
+#include <vector>
+#include <TFile.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TLorentzVector.h>
+#include <TCanvas.h>
+#include <TPad.h>
+#include <TPaveText.h>
+#include <TGraph.h>
+#include <TGraph2D.h>
+
+
+
+
+using namespace std;
+// using namespace reweight;
+// using namespace TopTree;
+
+bool test = false;
+bool testFit = false;
+bool testRead = true;
+
+
+string ConvertIntToString(int Number, int pad)
+{
+  ostringstream convert;
+  convert.clear();  // clear bits
+  convert.str(std::string());  // clear content
+  if ( pad > 1 ) { convert << std::setw(pad) << std::setfill('0');}
+  convert << Number;
+  return convert.str();
+}
+
+string MakeTimeStamp()
+{
+  time_t t = time(0);   // get time now
+  struct tm * now = localtime( & t );
+  
+  int year = now->tm_year - 100;  /// + 1900 to get current year
+  int month = now->tm_mon + 1;
+  int day = now->tm_mday;
+  int hour = now->tm_hour;
+  int min = now->tm_min;
+  //int sec = now->tm_sec;
+  
+  string year_str = ConvertIntToString(year, 2);
+  string month_str = ConvertIntToString(month, 2);
+  string day_str = ConvertIntToString(day, 2);
+  string hour_str = ConvertIntToString(hour, 2);
+  string min_str = ConvertIntToString(min, 2);
+  //string sec_str = ConvertIntToString(sec, 2);
+  
+  string date_str = year_str + month_str + day_str + "_" + hour_str + min_str;
+  return date_str;
+}
+
+
+int main (int argc, char *argv[])
+{
+  string dateString = MakeTimeStamp();
+  cout << "*********************************************" << endl;
+  cout << "***         Beginning of program          ***" << endl;
+  cout << "*********************************************" << endl;
+  cout << "*********************************************" << endl;
+  cout << "* Current time: " << dateString << "               *" << endl;
+  cout << "*********************************************" << endl;
+  
+  clock_t start = clock();
+  
+  
+  TGraph2D* graph = 0;
+  
+  string inputFileName = "LogLikelihoodFunction.root";  
+  TFile *inputFileLL = new TFile(inputFileName.c_str(), "read");
+  inputFileLL->GetObject("LogLikelihoodFunction", graph);
+  //graph = (TGraph2D*) inputFileLL->Get("LogLikelihoodFunction");
+  
+  graph->GetHistogram();
+  graph->SetMaxIter(500000000);
+  
+  cout << "N points: " << graph->GetN() << endl;
+  cout << "Value in (0.8,1.2): " << graph->Interpolate(1.4,1.2) << endl;
+  
+  TCanvas *c = new TCanvas("c", "c");
+  c->cd();
+  graph->Draw("colz");
+  c->SaveAs("testTGraphs.png");
+  delete c;
+  
+  
+  double time = ((double)clock() - start) / CLOCKS_PER_SEC;
+  cout << "It took us " << time << " s to run the program" << endl;
+  if ( time >= 60 )
+  {
+    int mins = time/60;
+    float secs = time - mins*60;
+    
+    if (mins >= 60 )
+    {
+      int hours = mins/60;
+      mins = mins - hours*60;
+      cout << "(This corresponds to " << hours << " hours, " << mins << " min and " << secs << " s)" << endl;
+    }
+    else
+      cout << "(This corresponds to " << mins << " min and " << secs << " s)" << endl;
+  }
+    
+  cout << "********************************************" << endl;
+  cout << "           End of the program !!            " << endl;
+  cout << "********************************************" << endl;
+  cout << " - Goodbye" << endl;
+
+  return 0;
+  
+}
