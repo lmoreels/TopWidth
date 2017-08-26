75175b362caa38e4b5f9d23641887cbe4692b1c6
Lieselotte Moreels <lieselotte.moreels@gmail.com>
2017-08-26 17:44:33 +0200

    Apply calibration curve and calculate uncertainties
    - Added parameters & function for calibration curve from Likelihood.cc
    - Calculate statistical uncertainties for absolute and relative shift
    - Write table with absolute shifts & statistical uncertainty

--- Systematics/CalculateSystematics.cc
+++ Systematics/CalculateSystematics.cc
@@ -19,10 +19,15 @@ bool verbose_ = true;
 string inputFileDir = "Systematics/temp/"; //"OutputLikelihood/170816_0947/";
 string listFileName = "list_syst.txt";
 
+const double calCurvePar_[2] = {0.00986675, 0.963861};
+const double calCurveParUnc_[2] = {0.0759725, 0.0301143};
+
 
 string line, lineList;
 string thisSyst;
 double thisWidth, thisWidthValue, thisWidthUnc;
+pair<double,double> thisCorrWidthValue;
+pair<double,double> testData;
 
 int nSys = -1;
 vector<string> systName;
@@ -30,6 +35,9 @@ vector<double> systWidthValue;
 vector<double> systWidthUnc;
 
 vector<double> systDifference, systRelDifference;
+vector<double> systDifferenceUnc, systRelDifferenceUnc;
+
+double nomVal, nomValUnc;
 
 int indexNom;
 int indexLepTrkUP, indexLepTrkDOWN, indexLepIdUP, indexLepIdDOWN, indexLepIsoUP, indexLepIsoDOWN, indexLepTrigUP, indexLepTrigDOWN, indexBTagUP, indexBTagDOWN, indexPuUP, indexPuDOWN, indexTopPt, indexLumiUP, indexLumiDOWN, indexRenFac1002, indexRenFac1003, indexRenFac1004, indexRenFac1005, indexRenFac1007, indexRenFac1009, indexIsrUP, indexIsrDOWN, indexFsrUP, indexFsrDOWN, indexHdampUP, indexHdampDOWN, indexTuneUP, indexTuneDOWN, indexCrErd, indexCrQcdErd, indexCrGluonMove, indexCrGluonMoveErd, indexMassUP, indexMassDOWN, indexHerwig;
@@ -40,7 +48,9 @@ bool foundUP(string name);
 bool foundDOWN(string name);
 void IndexSystematics();
 void TestIndexing();
-void WriteTable();
+std::pair<double,double> ApplyCalibrationCurve(double thisOutputWidth, double thisOutputWidthSigma);
+void WriteShiftTable();
+void WriteRelTable();
 
 bool fexists(const char *filename)
 {
@@ -87,7 +97,7 @@ int main()
   ifstream fileList(listFileName.c_str());
   while ( getline(fileList, lineList) )
   {
-    /// Get input from files
+    /// Get input from files in list
     ifstream fileIn(lineList);
     if (verbose_) cout << "Opening " << lineList << "..." << endl;
     while ( getline(fileIn, line) )
@@ -98,9 +108,11 @@ int main()
         iss >> thisSyst >> thisWidth >> thisWidthValue >> thisWidthUnc;
         //cout << thisSyst << "  " << thisWidthValue << "  " << thisWidthUnc << endl;
         
+        thisCorrWidthValue = ApplyCalibrationCurve(thisWidthValue, thisWidthUnc);
+        
         systName.push_back(thisSyst);
-        systWidthValue.push_back(thisWidthValue);
-        systWidthUnc.push_back(thisWidthUnc);
+        systWidthValue.push_back(thisCorrWidthValue.first);
+        systWidthUnc.push_back(thisCorrWidthValue.second);
       }
     }
     
@@ -122,18 +134,24 @@ int main()
   
   IndexSystematics();
   
-  double nomVal = systWidthValue[indexNom];
-  double diff, relDiff;
+  nomVal = systWidthValue[indexNom];
+  nomValUnc = systWidthUnc[indexNom];
+  double diff, uncDiff, relDiff, uncRelDiff;
   for (int iSys = 0; iSys < nSys; iSys++)
   {
     diff = systWidthValue[iSys] - nomVal;
     systDifference.push_back(diff);
+    uncDiff = TMath::Sqrt(systWidthUnc[iSys]*systWidthUnc[iSys] + nomValUnc*nomValUnc);
+    systDifferenceUnc.push_back(uncDiff);
     relDiff = 100*diff/nomVal;
     systRelDifference.push_back(relDiff);
+    uncRelDiff = TMath::Sqrt( systWidthUnc[iSys]*systWidthUnc[iSys]/(nomVal*nomVal) + nomValUnc*nomValUnc*systWidthValue[iSys]*systWidthValue[iSys]/pow(nomVal,4) );
+    systRelDifferenceUnc.push_back(uncRelDiff);
   }
   
   
-  WriteTable();
+  WriteShiftTable();
+  WriteRelTable();
   
   
   if (verbose_) cout << "End of the test program" << endl;
@@ -174,11 +192,16 @@ void ClearVars()
 {
   ClearIndices();
   
+  nomVal = 0.001;
+  nomValUnc = 0.001;
+  
   systName.clear();
   systWidthValue.clear();
   systWidthUnc.clear();
   systDifference.clear();
   systRelDifference.clear();
+  systDifferenceUnc.clear();
+  systRelDifferenceUnc.clear();
 }
 
 bool foundUP(string name)
@@ -286,7 +309,7 @@ void IndexSystematics()
       cerr << "Systematic " << systName[iSys] << " not found! Please update list..." << endl;
   }
   
-  if (verbose_) TestIndexing();
+  //if (verbose_) TestIndexing();
   
 }
 
@@ -310,16 +333,174 @@ void TestIndexing()
   cout << "indexRenFac1003  " << indexRenFac1003 << endl;
 }
 
-void WriteTable()
+std::pair<double,double> ApplyCalibrationCurve(double thisOutputWidth, double thisOutputWidthSigma)
 {
-  cout << "Add code to write LaTeX table here" << endl;
+  /// return 'thisInputWidth' and uncertainty
+  //  thisOutputWidth = Par_[0] + Par_[1] * thisInputWidth
+  
+  double thisInputWidth = (thisOutputWidth - calCurvePar_[0])/calCurvePar_[1];
+  double thisInputWidthSigma = TMath::Sqrt( thisOutputWidthSigma*thisOutputWidthSigma + calCurveParUnc_[0]*calCurveParUnc_[0] + calCurveParUnc_[1]*calCurveParUnc_[1]*thisInputWidth*thisInputWidth )/calCurvePar_[1];
+  
+  return std::pair<double,double>(thisInputWidth,thisInputWidthSigma);
+  
+}
+
+void WriteShiftTable()
+{
+  string space = "     ";
+  string hline = space+"\\hline";
+  string interline = space+"&&&&\\\\[-6pt]";
+  string headerextra = "[+3pt]";
+  string multicolBegin = "\\multicolumn{2}{r}{";
+  string multicolEnd = "$\\qquad$}";
+  
+  
+  ofstream fileOut("systematicsShiftFull.tex");
+  fileOut << "\\begin{table}[htp]" << endl;
+  fileOut << " \\caption{\\fixme{Put caption here}}" << endl;
+  fileOut << " \\begin{center}" << endl;
+  fileOut << "  \\begin{minipage}[p]{\\textwidth}" << endl;
+  fileOut << "   \\begin{center}" << endl;
+  fileOut << "    %\\hspace{-2.6cm}" << endl;
+  fileOut << "    {\\small" << endl;
+  fileOut << "    \\begin{tabular}{l S[table-format=1.4] S[table-format=1.4] S[table-format=1.4] S[table-format=1.4]}" << endl;  /// adapt!!
+  
+  fileOut << hline << endl;
+  fileOut << hline << endl;
+  fileOut << interline << endl;
+  fileOut << space << "\\multicolumn{3}{l}{Nominal value} & \\multicolumn{2}{l}{$" << fixed << setprecision(4) << nomVal << " \\pm " << nomValUnc << "$} \\\\" << headerextra << endl;
+  fileOut << hline << endl;
+  fileOut << interline << endl;
+  fileOut << space << "Systematic & \\multicolumn{2}{c}{$+1\\sigma$} & \\multicolumn{2}{c}{$-1\\sigma$} \\\\" << endl;
+  fileOut << space << " & {Shift} & {   Unc   } & {Shift} & {   Unc   } \\\\" << headerextra << endl;
+  fileOut << hline << endl;
+  
+  fileOut << interline << endl;
+  fileOut << space << "\\textit{Lepton SFs} &  &  &  &  \\\\" << endl;
+  if ( indexLepIdUP != -1 && indexLepIdDOWN != -1 )
+    fileOut << space << "\\tabsp Id & " << fixed << setprecision(4) << systDifference[indexLepIdUP] << " & " << systDifferenceUnc[indexLepIdUP] << " & " << systDifference[indexLepIdDOWN] << " & " << systDifferenceUnc[indexLepIdDOWN] << " \\\\" << endl;
+  if ( indexLepIsoUP != -1 && indexLepIsoDOWN != -1 )
+    fileOut << space << "\\tabsp Isolation & " << fixed << setprecision(4) << systDifference[indexLepIsoUP] << " & " << systDifferenceUnc[indexLepIsoUP] << " & " << systDifference[indexLepIsoDOWN] << " & " << systDifferenceUnc[indexLepIsoDOWN] << " \\\\" << endl;
+  if ( indexLepTrigUP != -1 && indexLepTrigDOWN != -1 )
+    fileOut << space << "\\tabsp Trigger & " << fixed << setprecision(4) << systDifference[indexLepTrigUP] << " & " << systDifferenceUnc[indexLepTrigUP] << " & " << systDifference[indexLepTrigDOWN] << " & " << systDifferenceUnc[indexLepTrigDOWN] << " \\\\" << endl;
+  if ( indexLepTrkUP != -1 && indexLepTrkDOWN != -1 )
+    fileOut << space << "\\tabsp Tracking & " << fixed << setprecision(4) << systDifference[indexLepTrkUP] << " & " << systDifferenceUnc[indexLepTrkUP] << " & " << systDifference[indexLepTrkDOWN] << " & " << systDifferenceUnc[indexLepTrkDOWN] << " \\\\" << endl;
+  
+  if ( indexBTagUP != -1 && indexBTagDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "\\bq~tagging SFs & " << fixed << setprecision(4) << systDifferenceUnc[indexBTagUP] << " & " << systDifferenceUnc[indexBTagUP] << " & " << systDifference[indexBTagDOWN] << " & " << systDifferenceUnc[indexBTagDOWN] << " \\\\" << endl;
+  }
+  
+  if ( indexPuUP != -1 && indexPuDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << fixed << setprecision(4) << "\\pileup\\ SFs & " << systDifference[indexPuUP] << " & " << systDifferenceUnc[indexPuUP] << " & " << systDifference[indexPuDOWN] << " & " << systDifferenceUnc[indexPuDOWN] << " \\\\" << endl;
+  }
+  
+  if ( indexLumiUP != -1 && indexLumiDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "Luminosity & " << fixed << setprecision(4) << systDifference[indexLumiUP] << " & " << systDifferenceUnc[indexLumiUP] << " & " << systDifference[indexLumiDOWN] << " & " << systDifferenceUnc[indexLumiDOWN] << " \\\\" << endl;
+  }
+  
+  if ( indexMassUP != -1 && indexMassDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "Top mass & +" << fixed << setprecision(4) << systDifference[indexMassUP] << " & " << systDifferenceUnc[indexMassUP] << " & " << systDifference[indexMassDOWN] << " & " << systDifferenceUnc[indexMassDOWN] << " \\\\" << endl;
+  }
+  
+  if ( indexTopPt != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "Top $\\pT$ reweighting & \\multicolumn{3}{r}{" << fixed << setprecision(4) << systDifference[indexTopPt] << "} & " << systDifferenceUnc[indexTopPt] << " \\\\" << endl;
+  }
+  
+  if ( indexRenFac1002 != -1 || indexRenFac1003 != -1 || indexRenFac1004 != -1 || indexRenFac1005 != -1 || indexRenFac1007 != -1 || indexRenFac1009 != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "\\textit{Renormalisation/factorisation scale} &  &  &  &  \\\\" << endl;
+    if ( indexRenFac1002 != -1 )
+      fileOut << space << "\\tabsp 1002 & \\multicolumn{3}{r}{" << systDifference[indexRenFac1002] << "} & " << systDifferenceUnc[indexRenFac1002] << " \\\\" << endl;
+    if ( indexRenFac1003 != -1 )
+      fileOut << space << "\\tabsp 1003 & \\multicolumn{3}{r}{" << systDifference[indexRenFac1003] << "} & " << systDifferenceUnc[indexRenFac1003] << " \\\\" << endl;
+    if ( indexRenFac1004 != -1 )
+      fileOut << space << "\\tabsp 1004 & \\multicolumn{3}{r}{" << systDifference[indexRenFac1004] << "} & " << systDifferenceUnc[indexRenFac1004] << " \\\\" << endl;
+    if ( indexRenFac1005 != -1 )
+      fileOut << space << "\\tabsp 1005 & \\multicolumn{3}{r}{" << systDifference[indexRenFac1005] << "} & " << systDifferenceUnc[indexRenFac1005] << " \\\\" << endl;
+    if ( indexRenFac1007 != -1 )
+      fileOut << space << "\\tabsp 1007 & \\multicolumn{3}{r}{" << systDifference[indexRenFac1007] << "} & " << systDifferenceUnc[indexRenFac1007] << " \\\\" << endl;
+    if ( indexRenFac1009 != -1 )
+      fileOut << space << "\\tabsp 1009 & \\multicolumn{3}{r}{" << systDifference[indexRenFac1009] << "} & " << systDifferenceUnc[indexRenFac1009] << " \\\\" << endl;
+  }
+  
+  if ( indexIsrUP != -1 && indexIsrDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "ISR & " << systDifference[indexIsrUP] << " & " << systDifferenceUnc[indexIsrUP] << " & " << systDifference[indexIsrDOWN] << " & " << systDifferenceUnc[indexIsrDOWN] << " \\\\" << endl;
+  }
+  if ( indexFsrUP != -1 && indexFsrDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "FSR & " << systDifference[indexFsrUP] << " & " << systDifferenceUnc[indexFsrUP] << " & " << systDifference[indexFsrDOWN] << " & " << systDifferenceUnc[indexFsrDOWN] << " \\\\" << endl;
+  }
+  if ( indexHdampUP != -1 && indexHdampDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "\\hdamp & " << systDifference[indexHdampUP] << " & " << systDifferenceUnc[indexHdampUP] << " & " << systDifference[indexHdampDOWN] << " & " << systDifferenceUnc[indexHdampDOWN] << " \\\\" << endl;
+  }
+  if ( indexTuneUP != -1 && indexTuneDOWN != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "Tune & " << systDifference[indexTuneUP] << " & " << systDifferenceUnc[indexTuneUP] << " & " << systDifference[indexTuneDOWN] << " & " << systDifferenceUnc[indexTuneDOWN] << " \\\\" << endl;
+  }
+  if ( indexCrErd != -1 || indexCrQcdErd != -1 || indexCrGluonMove != -1 || indexCrGluonMoveErd != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "\\textit{Colour reconnection} &  &  &  &  \\\\" << endl;
+    if ( indexCrErd != -1 )
+      fileOut << space << "\\tabsp MPI (ERD) & \\multicolumn{3}{r}{" << systDifference[indexCrErd] << "} & " << systDifferenceUnc[indexCrErd] << " \\\\" << endl;
+    if ( indexCrQcdErd != -1 )
+      fileOut << space << "\\tabsp QCD-based (ERD) & \\multicolumn{3}{r}{" << systDifference[indexCrQcdErd] << "} & " << systDifferenceUnc[indexCrQcdErd] << " \\\\" << endl;
+    if ( indexCrGluonMove != -1 )
+      fileOut << space << "\\tabsp Gluon move & \\multicolumn{3}{r}{" << systDifference[indexCrGluonMove] << "} & " << systDifferenceUnc[indexCrGluonMove] << " \\\\" << endl;
+    if ( indexCrGluonMoveErd != -1 )
+      fileOut << space << "\\tabsp Gluon move (ERD) & \\multicolumn{3}{r}{" << systDifference[indexCrGluonMoveErd] << "} & " << systDifferenceUnc[indexCrGluonMoveErd] << " \\\\" << endl;
+  }
+  
+  if ( indexHerwig != -1 )
+  {
+    fileOut << interline << endl;
+    fileOut << space << "Herwig & \\multicolumn{3}{r}{" << systDifference[indexHerwig] << "} & " << systDifferenceUnc[indexHerwig] << " \\\\" << endl;
+  }
+  
+  //....
+  
+  
+  fileOut << interline << endl;
+  fileOut << hline << endl;
+  fileOut << hline << endl;
+  
+  fileOut << "    \\end{tabular}}" << endl;
+  fileOut << "   \\end{center}" << endl;
+  fileOut << "  \\end{minipage}" << endl;
+  fileOut << " \\end{center}" << endl;
+  fileOut << " \\label{tab:systFullShift}" << endl;
+  fileOut << "\\end{table}%" << endl;
+  
+  cout << "Written table with absolute shifts + stat. uncertainties" << endl;
+  
+}
+
+void WriteRelTable()
+{
   string space = "     ";
   string hline = space+"\\hline";
   string interline = space+"&&\\\\[-6pt]";
   string headerextra = "[+3pt]";
   
   
-  ofstream fileOut("systematicsFull.tex");
+  ofstream fileOut("systematicsRelFull.tex");
   fileOut << "\\begin{table}[htp]" << endl;
   fileOut << " \\caption{\\fixme{Put caption here}}" << endl;
   fileOut << " \\begin{center}" << endl;
@@ -340,9 +521,9 @@ void WriteTable()
   if ( indexLepIdUP != -1 && indexLepIdDOWN != -1 )
     fileOut << space << "\\tabsp Id & +" << fixed << setprecision(4) << systRelDifference[indexLepIdUP] << " & " << fixed << setprecision(4) << systRelDifference[indexLepIdDOWN] << " \\\\" << endl;
   if ( indexLepIsoUP != -1 && indexLepIsoDOWN != -1 )
-    fileOut << space << "\\tabsp Isolation & +" << fixed << setprecision(4) << systRelDifference[indexLepIsoDOWN] << " & " << fixed << setprecision(4) << systRelDifference[indexLepIsoUP] << " \\\\" << endl;
+    fileOut << space << "\\tabsp Isolation & +" << fixed << setprecision(4) << systRelDifference[indexLepIsoUP] << " & " << fixed << setprecision(4) << systRelDifference[indexLepIsoDOWN] << " \\\\" << endl;
   if ( indexLepTrigUP != -1 && indexLepTrigDOWN != -1 )
-    fileOut << space << "\\tabsp Trigger & +" << fixed << setprecision(4) << systRelDifference[indexLepTrigDOWN] << " & " << fixed << setprecision(4) << systRelDifference[indexLepTrigUP] << " \\\\" << endl;
+    fileOut << space << "\\tabsp Trigger & +" << fixed << setprecision(4) << systRelDifference[indexLepTrigUP] << " & " << fixed << setprecision(4) << systRelDifference[indexLepTrigDOWN] << " \\\\" << endl;
   if ( indexLepTrkUP != -1 && indexLepTrkDOWN != -1 )
     fileOut << space << "\\tabsp Tracking & " << fixed << setprecision(4) << systRelDifference[indexLepTrkUP] << " & " << fixed << setprecision(4) << systRelDifference[indexLepTrkDOWN] << " \\\\" << endl;
   
@@ -445,7 +626,8 @@ void WriteTable()
   fileOut << "   \\end{center}" << endl;
   fileOut << "  \\end{minipage}" << endl;
   fileOut << " \\end{center}" << endl;
-  fileOut << " \\label{tab:systFull}" << endl;
+  fileOut << " \\label{tab:systFullRel}" << endl;
   fileOut << "\\end{table}%" << endl;
   
+  cout << "Written table with relative uncertainties" << endl;
 }
